6a1e1bf26f83aad9f2dfe0f1b7f41a40690cb927
ledragonseveille <ledragonseveille@gmail.com>
2019-01-26 21:16:30 +0100

    Add healing potion in the rooms

--- game.py
+++ game.py
@@ -480,7 +480,7 @@ class GameMap:
 
     def make_map(self, max_rooms, room_min_size, room_max_size,
                  map_width, map_height, player, entities,
-                 max_monsters_per_room):
+                 max_monsters_per_room, max_items_per_room):
         """ Procedural generation of the rooms """
 
         rooms = []
@@ -526,7 +526,10 @@ class GameMap:
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
-                self.place_entities(new_room, entities, max_monsters_per_room)
+                self.place_entities(new_room,
+                                    entities,
+                                    max_monsters_per_room,
+                                    max_items_per_room)
 
                 # Finally, append the new room to the list
                 rooms.append(new_room)
@@ -554,11 +557,16 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def place_entities(self, room, entities, max_monsters_per_room):
+    def place_entities(self,
+                       room,
+                       entities,
+                       max_monsters_per_room,
+                       max_items_per_room):
         """ Place entities in the rooms """
 
-        # Get a random number of monsters
+        # Get a random number of monsters and items
         number_of_monster = randint(0, max_monsters_per_room)
+        number_of_items = randint(0, max_items_per_room)
 
         for i in range(number_of_monster):
             # Choose a random location in the room
@@ -593,6 +601,21 @@ class GameMap:
                                      ai=ai_component)
 
                 entities.append(monster)
+
+        for i in range(number_of_items):
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities
+                        if entity.x == x and entity.y == y]):
+                item = Entity(x,
+                              y,
+                              '!',
+                              libtcod.violet,
+                              'Healing Potion',
+                              render_order=RenderOrder.ITEM)
+
+                entities.append(item)
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
@@ -646,6 +669,7 @@ def main():
     FOV_LIGHT_WALLS = True
     FOV_RADIUS = 6
     MAX_MONSTERS_PER_ROOM = 3
+    MAX_ITEMS_PER_ROOM = 2
 
     # Colors dict
     colors = {
@@ -691,7 +715,8 @@ def main():
                       MAP_HEIGHT,
                       player,
                       entities,
-                      MAX_MONSTERS_PER_ROOM)
+                      MAX_MONSTERS_PER_ROOM,
+                      MAX_ITEMS_PER_ROOM)
 
     # field of view init
     fov_recompute = True
